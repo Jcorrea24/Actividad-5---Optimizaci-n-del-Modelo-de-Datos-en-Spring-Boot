a0660f2a0ddfc9fb6fa5eb138b8a087a3999f7db
Julio Correa <jcorrea24@gmail.com>
2026-03-14 15:41:38 -0500

    Reorganizar estructura de carpetas en model

--- src/main/java/com/gestion/proyectos/sistema_proyectos/model/entity/Proyecto.java
+++ src/main/java/com/gestion/proyectos/sistema_proyectos/model/entity/Proyecto.java
@@ -4,9 +4,17 @@ import com.gestion.proyectos.sistema_proyectos.model.embeddable.ContactoCliente;
 import com.gestion.proyectos.sistema_proyectos.model.enums.EstadoProyecto;
 import com.gestion.proyectos.sistema_proyectos.model.enums.Prioridad;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "proyectos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Proyecto extends BaseEntity {
 
     @Column(nullable = false)
@@ -25,21 +33,4 @@ public class Proyecto extends BaseEntity {
 
     @Embedded
     private ContactoCliente contactoCliente;
-
-    public Proyecto() {}
-
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-
-    public String getDescripcion() { return descripcion; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
-
-    public EstadoProyecto getEstado() { return estado; }
-    public void setEstado(EstadoProyecto estado) { this.estado = estado; }
-
-    public Prioridad getPrioridad() { return prioridad; }
-    public void setPrioridad(Prioridad prioridad) { this.prioridad = prioridad; }
-
-    public ContactoCliente getContactoCliente() { return contactoCliente; }
-    public void setContactoCliente(ContactoCliente contactoCliente) { this.contactoCliente = contactoCliente; }
 }
